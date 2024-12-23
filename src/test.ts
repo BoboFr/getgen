9d36bda7558354ef743451c9fe682eec73203454
BoboFr <gaetan.biord@gmail.com>
2024-12-23 14:27:40 +0100

    Random stuff

--- src/test.ts
+++ src/test.ts
@@ -7,37 +7,13 @@ const agent = new Agent({
     debug: true
 });
 
-const promptAgent = new Agent({
-    modelName: 'qwen2:latest'
-});
-
-const customerIDParserTool: Tool = {
-    name: 'customer_parser_id',
-    description: 'Get the real customer ID',
-    parameters: [
-        {
-            name: 'customer_id',
-            type: 'string',
-            required: true,
-            description: 'The customer ID to get'
-        }
-    ],
-    execute: async (params) => {
-        return {
-            success: true,
-            data: "C"+String(params.customer_id).padStart(7, '0')
-        }
-    }
-};
-agent.addTools([]);
-
 const customerSchema = z.object({
     customer_id: z.string().describe("The customer's ID as 1234"),
     request: z.enum(["ACCOUNT_INFO", "ORDER_HISTORY", "UNKNOWN"]),
 });
 
 (async () => {
-    const prompt = `Bonjour, je suis le client 1452 et je souhaite obtenir les informations sur mon compte.`;
+    const prompt = `Bonjour, je suis le client 1452 et je souhaite obtenir les informations sur mon la guerre de centenaire.`;
     const res = await agent.executeWithSchema(customerSchema, {prompt});
 
     if(res.parsedResponse){
